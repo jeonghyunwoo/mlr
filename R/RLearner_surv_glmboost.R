b32eb2c2194fcc9367da0e1b7813ed8fc1e994f1
MariaErdmann <maria.erdmann@gmail.com>
2016-08-04 11:23:35 +0200

    Delected m param and remove part where the mstop is optimized via CV or AIC

--- R/RLearner_surv_glmboost.R
+++ R/RLearner_surv_glmboost.R
@@ -8,27 +8,18 @@ makeRLearner.surv.glmboost = function() {
       makeIntegerLearnerParam(id = "mstop", default = 100L, lower = 1L),
       makeNumericLearnerParam(id = "nu", default = 0.1, lower = 0, upper = 1),
       makeLogicalLearnerParam(id = "center", default = FALSE),
-      makeDiscreteLearnerParam(id = "m", default = "mstop", values = c("mstop", "cv")),
       makeLogicalLearnerParam(id = "use.formula", default = TRUE, when = "both")
     ),
-    par.vals = list(
-      family = mboost::CoxPH(),
-      m = "mstop",
-      use.formula = TRUE
-    ),
+    par.vals = list(family = mboost::CoxPH(), use.formula = TRUE),
     properties = c("numerics", "factors", "ordered", "weights", "rcens"),
     name = "Gradient Boosting with Componentwise Linear Models",
     short.name = "glmboost",
-    note = stri_paste(
-      "`family` has been set to `CoxPH()` by default.",
-      "Maximum number of boosting iterations is set via `mstop`, the actual number used for prediction is controlled by `m`.",
-      sep = " "
-    )
+    note = "`family` has been set to `CoxPH()` by default."
   )
 }
 
 #' @export
-trainLearner.surv.glmboost = function(.learner, .task, .subset, .weights = NULL, family, mstop, nu, m, use.formula, ...) {
+trainLearner.surv.glmboost = function(.learner, .task, .subset, .weights = NULL, family, mstop, nu, use.formula, ...) {
   ctrl = learnerArgsToControl(mboost::boost_control, mstop, nu)
   if (use.formula) {
     f = getTaskFormula(.task)
@@ -47,10 +38,6 @@ trainLearner.surv.glmboost = function(.learner, .task, .subset, .weights = NULL,
       mboost::glmboost(x = data$data, y = data$target, control = ctrl, weights = .weights, family = family, ...)
     }
     model = attachTrainingInfo(model, info)
-  }
-
-  if (m == "cv") {
-    mboost::mstop(model) = mboost::mstop(mboost::cvrisk(model, papply = lapply))
   }
   model
 }
